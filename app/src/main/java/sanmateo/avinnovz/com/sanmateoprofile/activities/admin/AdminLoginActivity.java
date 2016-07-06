c34775b220fa3636f34858c7c5fc9acceb33de27
Russel <rsbulanon@dynamicobjx.com>
2016-07-06 09:54:44 +0800

    Added product flavors
    - added admin flavor
    - added user flavor

---
+++ app/src/main/java/sanmateo/avinnovz/com/sanmateoprofile/activities/admin/AdminLoginActivity.java
@@ -0,0 +1,96 @@
+package sanmateo.avinnovz.com.sanmateoprofile.activities.admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.adapter.rxjava.HttpException;
+import sanmateo.avinnovz.com.sanmateoprofile.R;
+import sanmateo.avinnovz.com.sanmateoprofile.activities.BaseActivity;
+import sanmateo.avinnovz.com.sanmateoprofile.activities.MainActivity;
+import sanmateo.avinnovz.com.sanmateoprofile.activities.RegistrationActivity;
+import sanmateo.avinnovz.com.sanmateoprofile.fragments.LoginDialogFragment;
+import sanmateo.avinnovz.com.sanmateoprofile.helpers.ApiErrorHelper;
+import sanmateo.avinnovz.com.sanmateoprofile.helpers.ApiRequestHelper;
+import sanmateo.avinnovz.com.sanmateoprofile.helpers.AppConstants;
+import sanmateo.avinnovz.com.sanmateoprofile.helpers.LogHelper;
+import sanmateo.avinnovz.com.sanmateoprofile.interfaces.OnApiRequestListener;
+import sanmateo.avinnovz.com.sanmateoprofile.models.response.ApiError;
+import sanmateo.avinnovz.com.sanmateoprofile.models.response.AuthResponse;
+import sanmateo.avinnovz.com.sanmateoprofile.singletons.CurrentUserSingleton;
+
+/**
+ * Created by rsbulanon on 6/22/16.
+ */
+public class AdminLoginActivity extends BaseActivity implements OnApiRequestListener {
+
+    @BindView(R.id.tvCreateAccount) TextView tvCreateAccount;
+    private ApiRequestHelper apiRequestHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        AppConstants.IS_FACEBOOK_APP_INSTALLED = isFacebookInstalled();
+        apiRequestHelper = new ApiRequestHelper(this);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void showLoginDialogFragment() {
+        if (isNetworkAvailable()) {
+            final LoginDialogFragment loginDialogFragment = LoginDialogFragment.newInstance();
+            loginDialogFragment.setOnLoginListener(new LoginDialogFragment.OnLoginListener() {
+                @Override
+                public void OnLogin(String email, String password) {
+                    loginDialogFragment.dismiss();
+                    apiRequestHelper.authenticateUser(email,password);
+                }
+            });
+            loginDialogFragment.show(getFragmentManager(),"login");
+        } else {
+            showSnackbar(tvCreateAccount, AppConstants.WARN_CONNECTION);
+        }
+    }
+
+    @OnClick(R.id.tvCreateAccount)
+    public void showRegistrationPage() {
+        startActivity(new Intent(this, RegistrationActivity.class));
+        animateToLeft(this);
+    }
+
+    @Override
+    public void onApiRequestBegin(String action) {
+        if (action.equals(AppConstants.ACTION_LOGIN)) {
+            showCustomProgress("Logging in, Please wait...");
+        }
+    }
+
+    @Override
+    public void onApiRequestSuccess(String action, Object result) {
+        dismissCustomProgress();
+        if (action.equals(AppConstants.ACTION_LOGIN)) {
+            final CurrentUserSingleton currentUserSingleton = CurrentUserSingleton.newInstance();
+            final AuthResponse authResponse = (AuthResponse)result;
+            currentUserSingleton.setAuthResponse(authResponse);
+            startActivity(new Intent(this, MainActivity.class));
+            animateToLeft(this);
+            finish();
+        }
+    }
+
+    @Override
+    public void onApiRequestFailed(String action, Throwable t) {
+        dismissCustomProgress();
+        LogHelper.log("err","error in ---> " + action + " cause ---> " + t.getMessage());
+        if (t instanceof HttpException) {
+            if (action.equals(AppConstants.ACTION_LOGIN)) {
+                final ApiError apiError = ApiErrorHelper.parseError(((HttpException) t).response());
+                showConfirmDialog(action,"Login Failed", apiError.getMessage(),"Close","",null);
+            }
+        }
+    }
+}
