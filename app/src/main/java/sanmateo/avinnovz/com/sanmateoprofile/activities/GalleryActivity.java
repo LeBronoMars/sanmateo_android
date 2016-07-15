19a4f598399c49902b0a091a2c5b7d256f2c971d
Jovel Quiamco <jvquiamco@dynamicobjx.com>
2016-07-15 15:00:44 +0800

    Change Gallery temporary title

--- app/src/main/java/sanmateo/avinnovz/com/sanmateoprofile/activities/GalleryActivity.java
+++ app/src/main/java/sanmateo/avinnovz/com/sanmateoprofile/activities/GalleryActivity.java
@@ -19,6 +19,7 @@ public class GalleryActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
         ButterKnife.bind(this);
+        setToolbarTitle("Gallery");
         tvDescription.setText("In terms of geography, the town of  San Mateo is just a heartbeat away from Metro Manila.Barangay Banaba is its gateway, whether approaching it from either Nangka in Marikina City or Batasan Road in Quezon City. Upon driving through the arch above General Luna Highwaymarking the entry point to San Mateo, a marker can be seen by the side of the road saying something like you are now leaving the National Capital Region!  \\\\n\\\\nGeneral Luna Highway is San Mateo’s main thoroughfare. It must be a historic road where Andres Bonifacio could have passed by on his way to Pamitinan Cave. The legendary General Licerio Geronimo and his band of tiradores could have marched this road on their way to the victorious Battle of San Mateo.");
     }
 }
